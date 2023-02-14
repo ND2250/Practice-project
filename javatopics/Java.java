876764f6b800a0564c815e2addc827179f8ded6f
Nishant Dhanani <nishant.dhanani@gmail.com>
2023-02-14 21:15:01 +0000

    Practice project

---
+++ javatopics/Java.java
@@ -0,0 +1,18 @@
+package javatopics;
+
+public class Java {
+    // My package was name java before I rename with javatopics
+    public static void main(String[] args){
+        System.out.println("What is Java");
+        System.out.println("What is IDE");
+        System.out.println("what is class");
+        System.out.println("What is string");
+        System.out.println("Main method");
+        /*
+        My class name was Javatooics before
+        I renamed with Java
+         */
+    }
+
+
+}
---
+++ manualtopics/Manual.java
@@ -0,0 +1,22 @@
+package manualtopics;
+
+public class Manual {
+    //my package name was manual and I renamed the package as a manualtopics
+    public static void main(String[] args){
+        System.out.println("What is software");
+        System.out.println("What is software testing");
+        System.out.println("Techniques of software testing");
+        System.out.println("Unit testing");
+        System.out.println("Levels of software testing ");
+        System.out.println("What is BDD AND TDD");
+        System.out.println("Software development methods");
+        System.out.println(" what is change request");
+        System.out.println("What is HOTFIX");
+        System.out.println("Agile method");
+        /*
+        My classname was Manual before
+        I changed it with Manualtopics
+         */
+    }
+
+}
